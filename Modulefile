de7b90c7e75bb65ad5e56503061472e092ee918b
Justin Stoller <justin@puppetlabs.com>
2013-02-25 23:53:27 -0800

    initial commit

---
+++ Modulefile
@@ -0,0 +1,8 @@
+name    'justinstoller/rubyinstaller'
+version '0.0.1'
+source 'git://github.com/justinstoller/puppet-rubyinstaller.git'
+author 'justinstoller'
+license 'Apache License, Version 2.0'
+summary 'Install versions of Ruby on Windows'
+description 'Install the latest versions Ruby 1.8 and 1.9 on Windows systems using the RubyInstaller (rubyinstaller.org)'
+project_page 'https://github.com/justinstoller/puppet-rubyinstaller'
---
+++ manifests/installation.pp
@@ -0,0 +1,36 @@
+define rubyinstaller::installation(
+  $version     = $title,
+  $source      =  undef,
+  $destination = 'C:\packages'
+) {
+
+  if $source {
+    $location = $source
+
+  } else {
+    if $version =~ /19/ {
+      $pkg_name = "ruby-1.9.3-p374-i386-ming32.7z"
+      $install_dir = 'C:\Ruby193'
+    } elsif $version =~ /18/ {
+      $pkg_name = "ruby-1.8.7-p371-i386-ming32.7z"
+      $install_dir = 'C:\Ruby'
+    }
+    $location = "puppet:///modules/rubyinstaller/${pkg_name}"
+  }
+
+  $on_disk = "${destination}\\${pkg_name}"
+
+  file { $on_disk:
+    ensure => file,
+    source => $location,
+    mode   => '750',
+  }
+
+  exec { "Extract Ruby ${version}":
+    command => "7z x ${on_disk} -o ${install_dir}",
+    creates => $install_dir,
+    path    => 'C:\Chocolatey\bin',
+  }
+}
+
+}
